8d1a0c0a6b5df65cd77f363a6bad940a3e1678c6
amantiwariii <aman.t@tekonika.co>
2020-10-19 22:10:04 +0530

    some issues fixed

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Layout from '../components/core/layout';
-import Homepage from '../components/homepage'
+import Layout from '../components/Core/Layout';
+import Homepage from '../components/Homepage'
 
 const Home = () => {
   return <>

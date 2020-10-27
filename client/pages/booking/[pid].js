062ce4aab91cbaf6b3bd2babfd1a657478015238
amantiwariii <aman.t@tekonika.co>
2020-10-27 18:45:07 +0530

    pnr api integrated

---
+++ client/pages/booking/[pid].js
@@ -0,0 +1,27 @@
+import Layout from '../../components/Core/Layout';
+import { useEffect, useState } from 'react';
+import Private from '../../components/Core/Private';
+import { pnrDetails } from '../../actions/booking';
+import { withRouter } from 'next/router';
+
+const Booking = ({ data }) => {
+  return <>
+            <Layout>
+               <Private>
+               </Private>
+            </Layout>
+         </>
+}
+
+Booking.getInitialProps = ({ query }) => {
+    return  pnrDetails(query.pid)
+    .then(value => {
+       return { data: value}
+    })
+    .catch(err => {
+       return console.log(err)
+    })
+}
+
+
+export default withRouter(Booking);

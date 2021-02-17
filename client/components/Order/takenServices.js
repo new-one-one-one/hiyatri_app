3af9a7f56b754b851885cf2a72d66f24caaa6167
code-in-dark-sv <piyushohri297@gmail.com>
2021-02-17 11:35:19 +0530

    showed ui + server

--- client/components/Order/takenServices.js
+++ client/components/Order/takenServices.js
@@ -3,6 +3,7 @@ import {Paper} from "@material-ui/core";
 import useStyles from './style';
 
 const TakenServices = ({data}) => {
+    console.log(data, "data of taken services ")
   const toShow = data.porter_service_detail.porter_service_opted || data.cab_service_detail.cab_service_opted!==null;
   const classes = useStyles();
   const porter_total =  data.porter_service_detail.large_bags.total +data.porter_service_detail.medium_bags.total+data.porter_service_detail.small_bags.total

7229e4a12138f2750142c82ce7ee2bebcc814800
Prabhjot Singh <sprabhjot768@gmail.com>
2021-02-20 10:38:05 +0530

    refactored code

--- client/components/BulkBooking/BulkBooking.js
+++ client/components/BulkBooking/BulkBooking.js
@@ -104,22 +104,29 @@ const BulkBookings = () => {
         .then((res) => {
           setshowSpinner(false);
           toast.success("Upload successfull");
+          if(res.status===200){
+            createbulkbookings();
+          }
         })
         .catch((err) => {
           toast.error("upload fail");
         });
 
-      axios
-        .post(
-          `${process.env.NEXT_PUBLIC_API}/bulk_bookings_requests`,
-          uploadInformation
-        )
-        .then((res) => {
-          console.log(res);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+    
+
+        const createbulkbookings=()=>{
+          axios
+          .post(
+            `${process.env.NEXT_PUBLIC_API}/bulk_bookings_requests`,
+            uploadInformation
+          )
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+        }
     }
   };
 
